cc56dee3d08a48f5136aabcc3de720e7a045530f
daviddela <daviddela>
2021-11-21 13:29:09 +0100

    clean imports

--- src/test/java/com/daviddela/peluapi/web/CustomerControllerTest.java
+++ src/test/java/com/daviddela/peluapi/web/CustomerControllerTest.java
@@ -7,13 +7,10 @@ import com.daviddela.peluapi.services.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.is;
